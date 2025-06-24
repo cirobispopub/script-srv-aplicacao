b22c1f54ece5e41fd691209bf4c212813dc8b19b
cirobispopub <cirosuporte.cb@gmail.com>
2025-06-24 17:27:16 +0000

    Scritp srv aplicacao v.1

---
+++ script-srv-ap.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+echo "Atualizando o servidor....."
+
+apt-get update
+apt-get upgrade -y
+apt-get install apache2 -y
+apt-get install unzip -y
+
+echo "Baixando a aplicacao e descompatando..."
+
+cd /temp
+wget https://github.com/cirobispopub/quiz/archive/refs/heads/main.zip
+
+unzip main.zip
+cd quiz-main
+cp -R * /var/www/html/
+
+echo "Fim ....!"
+
